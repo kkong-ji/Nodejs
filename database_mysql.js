8e21f99b8e551ebf571743a1f371f94bb052bd25
Kong ji han <wisejohn950330@gmail.com>
2022-03-21 23:38:44 +0900

    nodejs와 mysql로 웹앱 제작하기

--- database_mysql.js
+++ database_mysql.js
@@ -20,8 +20,34 @@ conn.connect();
 // //   }
 // // });
 
-var sql = 'INSERT INTO topic (title, description, author) VALUES(?, ?, ?)';
-var params = ['Supervisor', 'Watcher', 'graphitte'];
+// var sql = 'INSERT INTO topic (title, description, author) VALUES(?, ?, ?)';
+// var params = ['Supervisor', 'Watcher', 'graphitte'];
+
+// conn.query(sql, params, function(err, rows, fields) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(rows);
+//   }
+// });
+// conn.end();
+
+/*
+var sql = 'UPDATE topic SET title=?, description=? WHERE id=?';
+var params = ['NPM', 'leezhe', 1];
+
+conn.query(sql, params, function(err, rows, fields) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(rows);
+  }
+});
+conn.end();
+*/
+
+var sql = 'DELETE FROM topic WHERE id=?';
+var params = [1];
 
 conn.query(sql, params, function(err, rows, fields) {
   if(err) {
