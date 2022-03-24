aaf89eba8252acd5efdc70646c843e10ddc6fb2e
Kong ji han <wisejohn950330@gmail.com>
2022-03-24 22:40:51 +0900

    nodejs와 mysql로 웹 앱 제작하기

--- app_mysql.js
+++ app_mysql.js
@@ -46,6 +46,43 @@ app.post('/topic/add', function(req, res){
   });
 })
 
+app.get(['/topic/:id/edit'], function(req, res){
+  var sql = 'SELECT id, title FROM topic';
+  conn.query(sql, function(err, topics, fields) {
+    var id = req.params.id;
+    if(id){
+      var sql = 'SELECT * FROM topic WHERE id=?';
+      conn.query(sql, [id], function(err, topic, fields){
+        if(err) {
+          console.log(err);
+          res.status(500).send('Internal Server Error');
+        } else {
+          res.render('edit', {topics:topics, topic:topic[0]});
+        }
+      });
+    } else {
+      console.log('There is no id.');
+      res.status(500).send('Internal Server Error');
+    }
+  });
+})
+app.post(['/topic/:id/edit'], function(req, res) {
+  var title = req.body.title;
+  var description = req.body.description;
+  var author = req.body.author;
+  var id = req.params.id;
+  var sql = 'UPDATE topic SET title=?, description=?, author=? WHERE id=?';
+  conn.query(sql, [title, description, author, id], function(err, result, fields) {
+    if(err) {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.redirect('/topic/' + id);
+    }
+  });
+});
+
+
 app.listen(3000, function() {
   console.log('Connected, 3000 port!');
 })
